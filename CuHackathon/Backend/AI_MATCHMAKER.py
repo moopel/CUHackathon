1a31ec64893b6e07628a5be7d8c0171357c0788f
Dante Mallozzi <dante.mallozzi@hotmail.com>
2025-03-07 20:41:38 -0500

    Added get_villain_name() prototype

--- CuHackathon/Backend/AI_MATCHMAKER.py
+++ CuHackathon/Backend/AI_MATCHMAKER.py
@@ -1,4 +1,12 @@
-import pandas as pd
+import pandas as pd # import pandas for reading in data
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 data = pd.read_csv('assets/Superheroes.csv')
 
+scaler = StandardScaler()
+numerical_features = ['Combat', 'Durability', 'Intelligence', 'Power', 'Strength', 'Speed'] # list of numeric data parsed
+print(data.columns)
+# data[numerical_features] = scaler.fit_transform(data[numerical_features])
+
+def get_villain_name():
+    pass
