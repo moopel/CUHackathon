b0d876bc7bb8f71b8e1e8a622d3adef3a326dbc8
Derek Smith <dereksmith32526@gmail.com>
2025-03-07 20:14:29 -0500

    Added new base project

---
+++ CuHackathon/C#Classes/PythonMedian.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuHackathon.C_Classes
+{
+    class PythonMedian
+    {
+    }
+}
