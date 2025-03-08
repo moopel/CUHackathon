fa4b278d013b42d8e88114e485d9537a207ff3f9
Derek Smith <dereksmith32526@gmail.com>
2025-03-07 20:29:43 -0500

    Changed Form name

--- CuHackathon/Forms/EntryForm.Designer.cs
+++ CuHackathon/Forms/EntryForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CuHackathon
 {
-    partial class Form1
+    partial class EntryForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // EntryForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1378, 944);
+            Name = "EntryForm";
+            Text = "Entry";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
     }
 }
--- CuHackathon/Forms/EntryForm.cs
+++ CuHackathon/Forms/EntryForm.cs
@@ -1,8 +1,8 @@
 namespace CuHackathon
 {
-    public partial class Form1 : Form
+    public partial class EntryForm : Form
     {
-        public Form1()
+        public EntryForm()
         {
             InitializeComponent();
         }
